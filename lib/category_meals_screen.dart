2eecfbe66021b11279e5df2b8eb6b1851321059b
Aleksandra Lisa Cybulska <aleksandra.cybulska@aequitas-group.pl>
2020-12-25 11:37:40 +0100

    Add named navigation

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
   @override
   Widget build(BuildContext context) {
+    final routeArg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final String categoryTitle = routeArg['title'];
+    // final String categoryId = routeArg['id'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
